c57436372e78574cf5e776503811d35c397122af
Ian Fox <iandouglasfox@gmail.com>
2025-05-17 18:41:31 -0700

    feat(IO): Basic readline prompting loop

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello human.");
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+import process from "node:process";
+
+const rl = readline.createInterface({ input, output });
+
+const loop = async () => {
+  const answer = await rl.question("> ");
+  loop();
+};
+
+loop();
+
+// doesn't get called with ctrl+c, but it feels wrong to leave it out
+process.on("exit", () => {
+  rl.close();
+});
